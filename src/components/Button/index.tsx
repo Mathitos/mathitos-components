8b4dd2d0aa72c615cb6ffd36081aef79f658a78b
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-06-25 11:19:41 -0300

    fix linter problem

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -1,27 +1,27 @@
-import React from "react";
-import cn from "classnames";
+import React from 'react'
+import cn from 'classnames'
 
-import "./button.scss";
+import './button.scss'
 
 export interface ButtonProps {
-  children: string;
-  className: string;
-  theme: string;
-  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void;
+  children: string
+  className: string
+  theme: string
+  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
 const Button = ({
   children,
-  className = "button-deafult",
-  theme = "primary",
-  onClick
+  className = 'button-deafult',
+  theme = 'primary',
+  onClick,
 }: ButtonProps) => (
   <div
-    className={cn("button", className, `button--${theme}`)}
+    className={cn('button', className, `button--${theme}`)}
     onClick={onClick}
   >
     {children}
   </div>
-);
+)
 
-export default Button;
+export default Button
--- src/stories/Button.stories.js
+++ src/stories/Button.stories.js
@@ -1,32 +1,32 @@
-import React from "react";
+import React from 'react'
 
-import { storiesOf } from "@storybook/react";
-import { action } from "@storybook/addon-actions";
+import { storiesOf } from '@storybook/react'
+import { action } from '@storybook/addon-actions'
 
-import Button from "../components/Button";
+import Button from '../components/Button'
 
-storiesOf("Button", module)
-  .add("with text", () => (
-    <Button onClick={action("clicked")}>
+storiesOf('Button', module)
+  .add('with text', () => (
+    <Button onClick={action('clicked')}>
       <span>Hello Button</span>
     </Button>
   ))
-  .add("with long text", () => (
-    <Button onClick={action("clicked")}>
+  .add('with long text', () => (
+    <Button onClick={action('clicked')}>
       <span>Helloooooooooooooooooooooooooooooooo Button</span>
     </Button>
   ))
-  .add("with some emoji", () => (
-    <Button onClick={action("clicked")}>
+  .add('with some emoji', () => (
+    <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯
       </span>
     </Button>
   ))
-  .add("with a lot of emoji", () => (
-    <Button onClick={action("clicked")}>
+  .add('with a lot of emoji', () => (
+    <Button onClick={action('clicked')}>
       <span role="img" aria-label="so cool">
         😀 😎 👍 💯 😂 🙀 👏
       </span>
     </Button>
-  ));
+  ))
