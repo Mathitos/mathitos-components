dc18255487c8617d170324a8ac81e26e1d626896
Matheus Anzzulin <matheus.c.anzzulin@gmail.com>
2019-06-25 15:57:09 -0300

    Add pages for each component, missing the required field be rendered from typescript

--- src/components/Button/index.tsx
+++ src/components/Button/index.tsx
@@ -3,21 +3,36 @@ import cn from 'classnames'
 
 import './button.scss'
 
-export interface ButtonProps {
-  children: string
-  className: string
-  theme: string
-  disabled: boolean
-  onClick: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
+interface Props {
+  children: any
+  /**
+   * Custom class name
+   */
+  className?: string
+  /**
+   * Theme for button
+   * @default "primary"
+   */
+  theme?: string
+  /**
+   * Button disabled status
+   */
+  disabled?: boolean
+  /**
+   * On click callback
+   * @default undefined
+   */
+  onClick?: (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => void
 }
 
-const Button = ({
+/** A button to use as example. */
+export const Button = ({
   children,
   className = 'button-deafult',
   theme = 'primary',
   disabled = false,
   onClick,
-}: ButtonProps) => (
+}: Props) => (
   <div
     className={cn(
       'button',
--- src/stories/Button.stories.tsx
+++ src/stories/Button.stories.tsx
@@ -2,22 +2,39 @@ import React from 'react'
 
 import { storiesOf } from '@storybook/react'
 import { action } from '@storybook/addon-actions'
+import { withInfo } from '@storybook/addon-info'
 
 import Button from '../components/Button'
 
-storiesOf('Button', module)
-  .add('Primary theme', () => (
+const stories = storiesOf('Button', module)
+
+stories.addDecorator(withInfo)
+
+stories.add(
+  'Primary theme',
+  () => (
     <Button onClick={action('clicked')}>
       <span>Hello Button</span>
     </Button>
-  ))
-  .add('Secondary theme', () => (
+  ),
+  { info: { inline: true } },
+)
+
+stories.add(
+  'Secondary theme',
+  () => (
     <Button theme="secondary" onClick={action('clicked')}>
       <span>Hello Button</span>
     </Button>
-  ))
-  .add('Disabled', () => (
+  ),
+  { info: { inline: true } },
+)
+stories.add(
+  'Disabled',
+  () => (
     <Button disabled onClick={action('clicked')}>
       <span>Hello Button</span>
     </Button>
-  ))
+  ),
+  { info: { inline: true } },
+)
